053a8c6a5ee7545b42ddf8242c3d98c41fb44ec4
Jackhein <moustiquejunior@gmail.com>
2023-02-21 17:12:35 +0100

    add detection for json file

--- check_modules_conflicts.sh
+++ check_modules_conflicts.sh
@@ -1,11 +1,11 @@
 #/usr/bin/sh
 
 cd ..
-echo "Check conflicts in CSV update file for DBC:";
-for csv in $(ls mod-*/data/dbc/*.csv | sed -e 's/\([^\/]\|\\\/\)*\///g' | sort | uniq -d); do
-	echo "	Check ID duplicate for all ${csv} files";
-	for dup in $(cat mod-*/data/dbc/${csv} | grep -o '^"[0-9]*"' | sort | uniq -d); do
+echo "Check conflicts in DBC update file:";
+for dbc in $(ls mod-*/data/dbc/*.{csv,json} | sed -e 's/\([^\/]\|\\\/\)*\/\|.json$\|.csv$//g' | sort | uniq -d); do
+	echo "	Check ID duplicate for all ${dbc} files";
+	for dup in $(cat mod-*/data/dbc/${dbc}.{csv,json} | grep -o '"ID":[0-9]*\|^"[0-9]*"' | grep -o '[0-9]*' | sort | uniq -d); do
 		echo "		Duplicate ID ${dup} in:";
-		grep -nH "^${dup}" mod-*/data/dbc/${csv} | sed -e 's/^/			/g';
+		grep -nHo "\"ID\":${dup}\|^\"${dup}\"" mod-*/data/dbc/${dbc}.* | sed -e 's/^\([^:]*:[^:]*\).*/			\1/g'
 	done;
 done;
