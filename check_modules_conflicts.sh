029fac2fd8c523f6aef2f5b713a44a4cb7ef755d
jackhein <moustiquejunior@gmail.com>
2023-03-18 23:45:11 +0100

    Remove error result from ls, clean/add message between duplicates listing.

--- check_modules_conflicts.sh
+++ check_modules_conflicts.sh
@@ -2,10 +2,11 @@
 
 cd ..
 echo "Check conflicts in DBC update file:";
-for dbc in $(ls mod-*/data/dbc/*.{csv,json} | sed -e 's/\([^\/]\|\\\/\)*\/\|.json$\|.csv$//g' | sort | uniq -d); do
-	echo "	Check ID duplicate for all ${dbc} files";
-	for dup in $(cat mod-*/data/dbc/${dbc}.{csv,json} | grep -o '"ID":[0-9]*\|^"[0-9]*"' | grep -o '[0-9]*' | sort | uniq -d); do
-		echo "		Duplicate ID ${dup} in:";
+for dbc in $(ls mod-*/data/dbc/*.{csv,json} 2>/dev/null | sed -e 's/\([^\/]\|\\\/\)*\/\|.json$\|.csv$//g' | sort | uniq -d); do
+	echo "	Check ID duplicate for all \"${dbc}\" files";
+	for dup in $(cat mod-*/data/dbc/${dbc}.{csv,json} 2>/dev/null | grep -o '"ID":[0-9]*\|^"[0-9]*"' | grep -o '[0-9]*' | sort | uniq -d); do
+		echo "		Duplicate ID \"${dup}\" in:";
 		grep -nHo "\"ID\":${dup}\|^\"${dup}\"" mod-*/data/dbc/${dbc}.* | sed -e 's/^\([^:]*:[^:]*\).*/			\1/g'
 	done;
+	echo "	Ended ID checking for all \"${dbc}\" files";
 done;
