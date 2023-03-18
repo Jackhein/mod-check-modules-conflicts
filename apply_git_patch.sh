6cb58bf6c4d69baa227e38795ef46887647c5f76
jackhein <moustiquejunior@gmail.com>
2023-03-18 23:43:48 +0100

    Remove "modules/" ahead in path of patch's files as git patch automatically assume current folder position.

--- apply_git_patch.sh
+++ apply_git_patch.sh
@@ -2,13 +2,13 @@
 
 cd ..
 echo "Search for git patch in modules:"
-for MODULE_NAME in $(ls mod-*/patch/*.patch | sed -e 's/\(\/[^\/]*\)*//g' | sort | uniq); do
+for MODULE_NAME in $(ls mod-*/patch/*.patch 2>/dev/null | sed -e 's/\(\/[^\/]*\)*//g' | sort | uniq); do
   echo ${MODULE_NAME}
-  for PATCH_NAME in $(ls ${MODULE_NAME}/patch/*.patch | sed -e 's/\([^\/]\|\\\/\)*\/\|.json$\|.csv$//g'); do
+  for PATCH_NAME in $(ls ${MODULE_NAME}/patch/*.patch 2>/dev/null | sed -e 's/\([^\/]\|\\\/\)*\/\|.json$\|.csv$//g'); do
     while true; do
       read -p "     Apply patch ${PATCH_NAME} found in ${MODULE_NAME}? Y/n"$'\n' yn
       case $yn in
-        [Yy]* ) git apply --ignore-space-change --ignore-whitespace modules/${MODULE_NAME}/${PATCH_NAME}.patch; break;;
+        [Yy]* ) git apply --ignore-space-change --ignore-whitespace ${MODULE_NAME}/patch/${PATCH_NAME}; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
       esac;
