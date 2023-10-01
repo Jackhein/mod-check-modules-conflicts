0d521f1527d375ccd18fd42fdded62b66a1bef65
jackhein <moustiquejunior@gmail.com>
2023-10-01 18:29:29 +0200

    update git patching aggreement

--- apply_git_patch.sh
+++ apply_git_patch.sh
@@ -9,15 +9,26 @@ for MODULE_NAME in $(
   for PATCH_NAME in $(
       ls modules/${MODULE_NAME}/patch/*.patch 2>/dev/null | sed -e 's/\([^\/]\|\\\/\)*\/\|.patch\|.csv$//g'
   ); do
-    while true; do
-      read -p "     Apply patch ${PATCH_NAME} found in ${MODULE_NAME}? Y/n"$'\n' yn
-      case $yn in
-        [Yy]* )
-          git apply --ignore-space-change --ignore-whitespace modules/${MODULE_NAME}/patch/${PATCH_NAME}.patch; break;;
-        [Nn]* )
-          break;;
-        * )
-          echo "Please answer yes or no.";;
+    while getopts ":y" opt; do
+      case $opt in
+        y)
+          echo "-a was triggered, Parameter: $OPTARG" >&2
+          ;;
+        \?)
+          ;;
+        :)
+          while true; do
+            read -p "     Apply patch ${PATCH_NAME} found in ${MODULE_NAME}? Y/n"$'\n' yn
+            case $yn in
+              [Yy]* )
+                git apply --ignore-space-change --ignore-whitespace modules/${MODULE_NAME}/patch/${PATCH_NAME}.patch; break;;
+              [Nn]* )
+                break;;
+              * )
+                echo "Please answer yes or no.";;
+            esac;
+          done;
+	  ;;
       esac;
     done;
   done;
