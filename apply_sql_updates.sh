5b2987e92d5c4ab58a5baa0603df49700bce3c62
jackhein <moustiquejunior@gmail.com>
2023-11-26 16:31:13 +0100

    Update insertion of databases data

--- apply_sql_updates.sh
+++ apply_sql_updates.sh
@@ -1,13 +1,31 @@
 #/usr/bin/sh
 
-echo "SQL updates:"
-declare -A TABLES
-read -p "     Host of database:"$'\n' HOST
-read -p "     User of database:"$'\n' USER
-read -p "     Password of database:"$'\n' PSWD
-read -p "     Auth table:"$'\n' TABLES["auth"]
-read -p "     Character table:"$'\n' TABLES["character"]
-read -p "     World table:"$'\n' TABLES['world']
+help()
+{
+   echo ""
+   echo "Usage: $0 -h [database address] -u [user] -p [password] -a [auth database] -c [character database] -w [world database]"
+   exit 1 # Exit script after printing help
+}
+
+declare -A DATABASES
+while getopts "h:u:p:a:c:w:" opt
+do
+   case "$opt" in
+      h ) HOST="$OPTARG" ;;
+      u ) USER="$OPTARG" ;;
+      p ) PSWD="$OPTARG" ;;
+      a ) DATABASES["auth"]="$OPTARG" ;;
+      c ) DATABASES["character"]="$OPTARG" ;;
+      w ) DATABASES["world"]="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+if (( ${#HOST} == 0 || ${#USER} == 0 || ${#PSWD} == 0 || (${#DATABASES["auth"]} == 0 && ${#DATABASES["character"]} == 0 && ${#DATABASES["world"]} == 0) ))
+then
+   echo "Some or all of the parameters are empty";
+   help
+fi
 
 cd ..
 echo "Search for sql updates in modules:"
@@ -21,9 +39,9 @@ for MODULE_NAME in $(
     echo ${TABLE_NAME}
     for SQL in $(find ${MODULE_NAME}/sql/${TABLE_NAME}/ -type f -name '*.sql'); do
       while true; do
-        read -p "     Apply sql update from module \"$(echo ${SQL} | sed -e 's/\([^\/]*\/\)*//g')\" for table \"${TABLES[${TABLE_NAME}]}\" found in \"${MODULE_NAME}\"? Y/n"$'\n' yn
+        read -p "     Apply sql update from module \"$(echo ${SQL} | sed -e 's/\([^\/]*\/\)*//g')\" for table \"${DATABASES[${TABLE_NAME}]}\" found in \"${MODULE_NAME}\"? Y/n"$'\n' yn
         case $yn in
-          [Yy]* ) mysql -u${USER} -h${HOST} -D${TABLES[${TABLE_NAME}]} -p${PSWD} < ${SQL}; break;;
+          [Yy]* ) mysql -u${USER} -h${HOST} -D${DATABASES[${TABLE_NAME}]} -p${PSWD} < ${SQL}; break;;
           [Nn]* ) break;;
           * ) echo "Please answer yes or no.";;
         esac;
