ca708e00db07861824222b264576817946ef11c4
jackhein <moustiquejunior@gmail.com>
2023-11-28 18:30:12 +0100

    update message conf copy

--- apply_conf_copy.sh
+++ apply_conf_copy.sh
@@ -14,12 +14,12 @@ help()
 while getopts "p:" opt
 do
    case "$opt" in
-      p ) PATH="$OPTARG" ;;
+      p ) CONF_PATH="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
-if (( ${#PATH} == 0 ))
+if ${#CONF_PATH} == 0
 then
    echo -e "${ERR}Entry a valid path${END}";
    help
@@ -31,14 +31,14 @@ for MODULE_NAME in $(
   ls $(find mod-*/ -type f -name '*conf.dist') 2>/dev/null | grep -Eo ".*/" | sed -e 's/\/.*//g' | sort | uniq
 ); do
   echo -e "Current module: ${INF}${MODULE_NAME}${END}"
-  for CONF_PATH in $(
+  for MOD_PATH in $(
     find ${MODULE_NAME}/ -type f -name '*.conf.dist'
   ); do
-    CONF_NAME=$(echo ${CONF_PATH} | grep -Eo "/*.conf")
+    CONF_NAME=$(echo ${MOD_PATH} | grep -Eo "[^/].+\.conf\.dist^" | grep -Eo ".*\.conf")
     while true; do
       read -p "$(echo -e "\t\tCopy from module ${INF}\"${MODULE_NAME}\"${END} conf file ${INF}\"${CONF_NAME}\"${END}? ${INF}Y${END}/${INF}n${END}")"$'\n' yn
       case $yn in
-        [Yy]* ) echo "cp ${CONF_PATH} ${PATH}${CONF_NAME}"; break;;
+        [Yy]* ) echo "cp ${MOD_PATH} ${PATH}${CONF_NAME}"; break;;
         [Nn]* ) break;;
         * ) echo -e "Please answer ${INF}y${END}es or ${INF}n${END}o.";;
       esac;
