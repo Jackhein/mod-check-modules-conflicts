a15557b7a6812fa69bea0d24992ae6d01f2cb55a
jackhein <moustiquejunior@gmail.com>
2024-01-15 22:38:38 +0100

    Instead of applying sql, copy file to directory containing custom sql files

--- apply_sql_updates.sh
+++ apply_sql_updates.sh
@@ -4,34 +4,10 @@ ERR="\e[31m"
 INF="\e[36m"
 END="\e[0m"
 
-help()
-{
-   echo ""
-   echo -e "${INF}Usage: $0 -h [database address] -u [user] -p [password] -a [auth database] -c [character database] -w [world database] (-y for automatic use)${END}"
-   exit 1 # Exit script after printing help
-}
-
-AUTO=0
 declare -A DATABASES
-while getopts "h:u:p:a:c:w:y" opt
-do
-   case "$opt" in
-      h ) HOST="$OPTARG" ;;
-      u ) USER="$OPTARG" ;;
-      p ) PSWD="$OPTARG" ;;
-      a ) DATABASES["auth"]="$OPTARG" ;;
-      c ) DATABASES["character"]="$OPTARG" ;;
-      w ) DATABASES["world"]="$OPTARG" ;;
-      y ) AUTO=1 ;;
-      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
-   esac
-done
-
-if (( ${#HOST} == 0 || ${#USER} == 0 || ${#PSWD} == 0 || (${#DATABASES["auth"]} == 0 && ${#DATABASES["character"]} == 0 && ${#DATABASES["world"]} == 0) ))
-then
-   echo -e "${ERR}Some or all of the parameters are empty${END}";
-   help
-fi
+DATABASES["auth"]="db_auth"
+DATABASES["character"]="db_characters"
+DATABASES["world"]="db_world"
 
 cd ..
 echo "Search for sql updates in modules:"
@@ -52,7 +28,7 @@ for MODULE_NAME in $(
         while true; do
           read -p "$(echo -e "\t\tApply from module ${INF}\"${MODULE_NAME}\"${END} to database ${INF}\"${DATABASE_NAME}\"${END} sql update ${INF}\"$(echo ${SQL} | sed -e 's/\([^\/]*\/\)*//g')\"${END}? ${INF}Y${END}/${INF}n${END}")"$'\n' yn
           case $yn in
-              [Yy]* ) mysql -u${USER} -h${HOST} -D${DATABASE_NAME} -p${PSWD} --default-character-set=utf8mb4 --execute="SET NAMES utf8mb4; SOURCE ${SQL};"; break;;
+              [Yy]* ) cp ${SQL} /wotlk/data/sql/updates/${DATABASE_NAME}; break;;
               [Nn]* ) break;;
               * ) echo -e "Please answer ${INF}y${END}es or ${INF}n${END}o.";;
           esac;
