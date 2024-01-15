7c23bbc61a2f79585ef3328ad4c9e305c940d2b1
jackhein <moustiquejunior@gmail.com>
2024-01-15 23:24:47 +0100

    Update help message and automatic process for apply_sql_updates.sh

--- apply_sql_updates.sh
+++ apply_sql_updates.sh
@@ -1,9 +1,12 @@
 #/usr/bin/sh
 
+#Color code
 ERR="\e[31m"
 INF="\e[36m"
 END="\e[0m"
 
+#Path and folders where to copy sql files
+CUSTOM_PATH="data/sql/custom/"
 declare -A DATABASES
 DATABASES["auth"]="db_auth"
 DATABASES["character"]="db_characters"
@@ -11,6 +14,24 @@ DATABASES["world"]="db_world"
 
 #Ignore following modules as their path are already checked (see list under apps/ci/ci-install-modules.sh)
 IGNORE_CASE="(mod-eluna|mod-autobalance|mod-ah-bot|mod-anticheat|mod-bg-item-reward|mod-cfbg|mod-chat-transmitter|mod-cta-switch|mod-desertion-warnings|mod-duel-reset|mod-ip-tracker|mod-low-level-arena|mod-low-level-rbg|mod-multi-client-check|mod-pvp-titles|mod-pvpstats-announcer|mod-queue-list-cache|mod-server-auto-shutdown|mod-transmog|mod-progression-system)"
+
+#Automatic applyance
+AUTO=0
+
+help()
+{
+  echo ""
+  echo -e "${INF}Usage: $0 -h (show help) |-y (for automatic use)${END}"
+  exit 1 # Exit script after printing help
+}
+
+while getopts "h:u:p:a:c:w:y" opt
+do
+  case "$opt" in
+    y ) AUTO=1 ;;
+    ? ) help ;; # Print helpFunction in case parameter is non-existent
+  esac
+done
 
 cd ..
 echo "Search for sql updates in modules:"
@@ -26,12 +47,13 @@ for MODULE_NAME in $(
     for SQL in $(find ${DATABASE_PATH} -type f -name '*.sql'); do
       if (( AUTO==1 ))
       then
-        mysql -u${USER} -h${HOST} -D${DATABASE_NAME} -p${PSWD} < ${SQL};
+        echo -e "\t\tApply from module ${INF}\"${MODULE_NAME}\"${END} to database ${INF}\"${DATABASE_NAME}\"${END} sql update ${INF}\"$(echo ${SQL} | sed -e 's/\([^\/]*\/\)*//g')\"${END}"
+        cp ${SQL} ../${CUSTOM_PATH}${DATABASE_NAME};
       else
         while true; do
           read -p "$(echo -e "\t\tApply from module ${INF}\"${MODULE_NAME}\"${END} to database ${INF}\"${DATABASE_NAME}\"${END} sql update ${INF}\"$(echo ${SQL} | sed -e 's/\([^\/]*\/\)*//g')\"${END}? ${INF}Y${END}/${INF}n${END}")"$'\n' yn
           case $yn in
-              [Yy]* ) cp ${SQL} ../data/sql/custom/${DATABASE_NAME}; break;;
+              [Yy]* ) cp ${SQL} ../${CUSTOM_PATH}${DATABASE_NAME}; break;;
               [Nn]* ) break;;
               * ) echo -e "Please answer ${INF}y${END}es or ${INF}n${END}o.";;
           esac;
