93f5f8d79db677924264fb26f0c4501da61792c8
jackhein <moustiquejunior@gmail.com>
2024-01-16 19:33:41 +0100

    now we copy sql file instead of run it, remove unused part in conf application, add automatic process for dbc copy

--- apply_conf_copy.sh
+++ apply_conf_copy.sh
@@ -7,14 +7,6 @@ END="\e[0m"
 #Ignore following modules as their path are already checked (see list under apps/ci/ci-install-modules.sh)
 IGNORE_CASE="(mod-eluna|mod-autobalance|mod-ah-bot|mod-anticheat|mod-bg-item-reward|mod-cfbg|mod-chat-transmitter|mod-cta-switch|mod-desertion-warnings|mod-duel-reset|mod-ip-tracker|mod-low-level-arena|mod-low-level-rbg|mod-multi-client-check|mod-pvp-titles|mod-pvpstats-announcer|mod-queue-list-cache|mod-server-auto-shutdown|mod-transmog|mod-progression-system)"
 
-#Path and folders where to copy sql files
-CUSTOM_PATH="data/sql/custom/"
-declare -A DATABASES
-DATABASES["auth"]="db_auth"
-DATABASES["character"]="db_characters"
-DATABASES["world"]="db_world"
-
-
 #Automatic applyance
 AUTO=0
 
@@ -52,13 +44,13 @@ for MODULE_NAME in $(
     CONF_NAME=$(echo ${MOD_PATH} | grep -Eo "[^/]+\.conf(|.dist)$")
     if (( AUTO==1 ))
       then
-        echo -e "\tCopy from module ${INF}\"${MODULE_NAME}\"${END} conf file ${INF}\"${CONF_NAME}\"${END}"
+        echo -e "\tCopy from module ${INF}\"${MODULE_NAME}\"${END} conf file ${INF}\"${CONF_NAME}\"${END}";
         cp ${MOD_PATH} ${CONF_PATH}${CONF_NAME};
       else
         while true; do
           read -p "$(echo -e "\tCopy from module ${INF}\"${MODULE_NAME}\"${END} conf file ${INF}\"${CONF_NAME}\"${END}? ${INF}Y${END}/${INF}n${END}")"$'\n' yn
           case $yn in
-            [Yy]* ) cp ${MOD_PATH} ${CONF_PATH}${CONF_NAME}; break;;
+            [Yy]* ) echo "cp ${MOD_PATH} ${CONF_PATH}${CONF_NAME}"; break;;
             [Nn]* ) break;;
             * ) echo -e "Please answer ${INF}y${END}es or ${INF}n${END}o.";;
           esac;
--- apply_dbc_copy.sh
+++ apply_dbc_copy.sh
@@ -4,10 +4,16 @@ ERR="\e[31m"
 INF="\e[36m"
 END="\e[0m"
 
+#Ignore following modules as their path are already checked (see list under apps/ci/ci-install-modules.sh)
+IGNORE_CASE="(mod-eluna|mod-autobalance|mod-ah-bot|mod-anticheat|mod-bg-item-reward|mod-cfbg|mod-chat-transmitter|mod-cta-switch|mod-desertion-warnings|mod-duel-reset|mod-ip-tracker|mod-low-level-arena|mod-low-level-rbg|mod-multi-client-check|mod-pvp-titles|mod-pvpstats-announcer|mod-queue-list-cache|mod-server-auto-shutdown|mod-transmog|mod-progression-system)"
+
+#Automatic applyance
+AUTO=0
+
 help()
 {
    echo ""
-   echo -e "${INF}Usage: $0 -p [dbc files path]${END}"
+   echo -e "${INF}Usage: $0 -p \"dbc/file/path\" [-h (show help)] [-y (for automatic use)]${END}"
    exit 1 # Exit script after printing help
 }
 
@@ -15,6 +21,7 @@ while getopts "p:" opt
 do
    case "$opt" in
       p ) DBC_PATH="$OPTARG" ;;
+      y ) AUTO=1 ;;
       ? ) help ;; # Print helpFunction in case parameter is non-existent
    esac
 done
@@ -35,13 +42,19 @@ for MODULE_NAME in $(
     find ${MODULE_NAME}/ -type f -name '*.dbc'
   ); do
     DBC_NAME=$(echo ${MOD_PATH} | grep -Eo "[^/]+\.dbc")
-    while true; do
-      read -p "$(echo -e "\tCopy from module ${INF}\"${MODULE_NAME}\"${END} dbc file ${INF}\"${DBC_NAME}\"${END}? ${INF}Y${END}/${INF}n${END}")"$'\n' yn
-      case $yn in
-        [Yy]* ) cp ${MOD_PATH} ${DBC_PATH}${DBC_NAME}; break;;
-        [Nn]* ) break;;
-        * ) echo -e "Please answer ${INF}y${END}es or ${INF}n${END}o.";;
-      esac;
-    done;
+    if (( AUTO==1 ))
+      then
+        echo -e "\tCopy from module ${INF}\"${MODULE_NAME}\"${END} dbc file ${INF}\"${DBC_NAME}\"${END}";
+        cp ${MOD_PATH} ${DBC_PATH}${DBC_NAME};
+      else
+        while true; do
+          read -p "$(echo -e "\tCopy from module ${INF}\"${MODULE_NAME}\"${END} dbc file ${INF}\"${DBC_NAME}\"${END}? ${INF}Y${END}/${INF}n${END}")"$'\n' yn
+          case $yn in
+            [Yy]* ) cp ${MOD_PATH} ${DBC_PATH}${DBC_NAME}; break;;
+            [Nn]* ) break;;
+            * ) echo -e "Please answer ${INF}y${END}es or ${INF}n${END}o.";;
+          esac;
+        done;
+      fi;
   done;
 done;
